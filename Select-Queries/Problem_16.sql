626717c9db209b51ed2fecf986a0f9a9e9a096c2
Krishna Vora <kpvora05@gmail.com>
2026-01-21 13:56:53 +0530

    Add files via upload

---
+++ Select-Queries/Problem_16.sql
@@ -0,0 +1,9 @@
+-- Problem: Weather Observation Station 11
+--Platform: HackerRank
+--Difficulty: Easy
+
+Question: Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY NOT REGEXP '^[AEIOUaeiou]'
+OR CITY NOT REGEXP '[AEIOUaeiou]$';
