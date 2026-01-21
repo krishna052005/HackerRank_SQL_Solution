531b106adfa98b71fe0268bcd4314ff143bb9546
Krishna Vora <kpvora05@gmail.com>
2026-01-21 13:57:47 +0530

    Add files via upload

---
+++ Select-Queries/Problem_18.sql
@@ -0,0 +1,9 @@
+-- Problem: Employee Names
+--Platform: HackerRank
+--Difficulty: Easy
+
+Question: Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+SELECT name FROM Employee
+ORDER BY name asc;
+
