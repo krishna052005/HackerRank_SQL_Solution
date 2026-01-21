2322ab50d859dd201ee76c6ae765d50c7c3dffe7
Krishna Vora <kpvora05@gmail.com>
2026-01-22 00:42:50 +0530

    Add files via upload

---
+++ Aggegration-Question/Problem_1.sql
@@ -0,0 +1,8 @@
+-- Problem: Revising Aggregations - The Count Function
+--Platform: HackerRank
+--Difficulty: Easy
+
+Question: Query a count of the number of cities in CITY having a Population larger than 100000.
+
+SELECT COUNT(NAME) FROM CITY
+WHERE POPULATION>100000
