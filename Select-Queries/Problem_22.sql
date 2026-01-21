64366306a3d254c136e72b6b8ea8e470041287cc
Krishna Vora <kpvora05@gmail.com>
2026-01-22 00:41:29 +0530

    Add files via upload

---
+++ Select-Queries/Problem_22.sql
@@ -0,0 +1,20 @@
+-- Problem: Occupations
+--Platform: HackerRank
+--Difficulty: Medium
+
+Question: Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output should consist of four columns (Doctor, Professor, Singer, and Actor) in that specific order, with their respective names listed alphabetically under each column.
+
+Note: Print NULL when there are no more names corresponding to an occupation.
+
+SELECT
+    MAX(CASE WHEN Occupation = 'Doctor' THEN Name END) AS Doctor,
+    MAX(CASE WHEN Occupation = 'Professor' THEN Name END) AS Professor,
+    MAX(CASE WHEN Occupation = 'Singer' THEN Name END) AS Singer,
+    MAX(CASE WHEN Occupation = 'Actor' THEN Name END) AS Actor
+FROM (
+    SELECT Name, Occupation,
+           ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) AS rn
+    FROM OCCUPATIONS
+) AS t
+GROUP BY rn
+ORDER BY rn;
