ae0952f48ab7b5a97ae663a462bfea9552a8e915
Krishna Vora <kpvora05@gmail.com>
2026-01-23 13:08:04 +0530

    Add files via upload

---
+++ Aggegration-Question/Problem_6.sql
@@ -0,0 +1,8 @@
+-- Problem: Population Density Difference
+--Platform: HackerRank
+--Difficulty: Easy
+
+Question: Query the difference between the maximum and minimum populations in CITY.
+
+SELECT max(POPULATION)-min(POPULATION) as Difference
+FROM CITY;
