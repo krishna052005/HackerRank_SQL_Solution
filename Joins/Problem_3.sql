1184619a4b63944fb606bee73bda313dc5ea363e
Krishna Vora <kpvora05@gmail.com>
2026-01-25 23:43:38 +0530

    Add files via upload

---
+++ Joins/Problem_3.sql
@@ -0,0 +1,12 @@
+-- Problem: Average Population of Each Continent
+--Platform: HackerRank
+--Difficulty: Easy
+
+Question: Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+SELECT COUNTRY.CONTINENT, FLOOR(AVG(CITY.POPULATION)) AS avg_population
+FROM CITY
+INNER JOIN COUNTRY
+ON CITY.COUNTRYCODE = COUNTRY.CODE
+GROUP BY COUNTRY.CONTINENT;
